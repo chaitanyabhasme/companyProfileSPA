d50da0cd3632a9cc3c5aff7173480efea931f42f
Chaitanya Bhasme <chaitanya6790@gmail.com>
2017-01-21 23:13:50 -0500

    complete the app

--- app.js
+++ app.js
@@ -6,11 +6,37 @@ app.config(['$routeProvider', function ($routeProvider) {
       templateUrl: 'main.html',
       controller: 'MainCtrl'
     })
+    .when('/about', {
+      templateUrl: 'about.html',
+      controller: 'MainCtrl'
+    })
+    .when('/services', {
+      templateUrl: 'services.html',
+      controller: 'ServicesCtrl'
+    })
+    .when('/contact', {
+      templateUrl: 'contact.html',
+      controller: 'ContactCtrl'
+    })
     .otherwise({
       redirectTo: '/main'
     });
 }]);
 
-app.controller('MainCtrl', [function () {
-  console.log('Main controller called.');
+app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('services.json').then(function (response) {
+    $scope.services = response.data;
+  });
+}]);
+
+app.controller('ServicesCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('services.json').then(function (response) {
+    $scope.services = response.data;
+  });
+}]);
+
+app.controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('locations.json').then(function (response) {
+    $scope.locations = response.data;
+  })
 }]);
